951c8aa9a87cda8c109b88f4cd003aa85d38b7a2
Ubuntu <ubuntu@ip-172-31-85-196.ec2.internal>
2022-05-30 18:31:05 +0000

    Agrega espacio para pasar prueba estática

--- src/todoList.py
+++ src/todoList.py
@@ -8,7 +8,7 @@ from botocore.exceptions import ClientError
 
 
 def get_table(dynamodb=None):
-    print("Valor dynamodb: ",dynamodb)
+    print("Valor dynamodb: ", dynamodb)
     print("endpoint_override: ", os.environ['ENDPOINT_OVERRIDE']) 
     if not dynamodb:
         URL = os.environ['ENDPOINT_OVERRIDE']
