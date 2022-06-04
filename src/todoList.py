fead0e37576017c7c6c6284aed6f3b8a1e0ee0ad
avegamx@gmail.com <avegamx@gmail.com>
2022-06-04 05:06:54 +0000

    Se arreglan errores de identacion

--- src/todoList.py
+++ src/todoList.py
@@ -159,15 +159,15 @@ def get_translate(key, lg, dynamodb=None):
     # translate content
     try:
         translate = boto3.client(service_name='translate',
-                    region_name='us-east-1', use_ssl=True)
+                                 region_name='us-east-1', use_ssl=True)
         result = table.get_item(
             Key={
                 'id': key
             }
         )
         traduccion = translate.translate_text(Text=result['Item']['text'],
-                                               SourceLanguageCode='auto',
-                                               TargetLanguageCode=lg)
+                                              SourceLanguageCode='auto',
+                                              TargetLanguageCode=lg)
         print(traduccion)
         result['Item']["text"] = traduccion.get('TranslatedText')
 
